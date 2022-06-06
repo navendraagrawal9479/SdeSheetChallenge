4b07cf33fb434cb88ac472a5c4fcb5da8fdb147f
navendraagrawal9479 <75437871+navendraagrawal9479@users.noreply.github.com>
2022-06-06 17:51:58 +0530

    Add files via upload

---
+++ 19.Pair_Sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+using namespace std;
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+
+  // Write your code here.
+   vector<vector<int>>ans;
+   for(int i=0;i<arr.size();i++)
+   {
+       for(int j=i+1;j<arr.size();j++)
+       {
+           if(arr[i]+arr[j]==s)  
+               ans.push_back({min(arr[i],arr[j]),max(arr[i],arr[j])});
+       }
+   }
+   sort(ans.begin(),ans.end());
+   return ans;
+}
